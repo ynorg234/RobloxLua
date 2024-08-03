fa647c45ed39764c2238121dc274f68d5f5215f1
ynorg234 <119822347+ynorg234@users.noreply.github.com>
2024-08-03 13:39:22 -0400

    fix even more

--- main.lua
+++ main.lua
@@ -56,102 +56,100 @@ G2L["2"]["Size"] = UDim2.new(0, 249, 0, 116);
 G2L["2"]["Position"] = UDim2.new(0.34727, 0, 0.37702, 0);
 G2L["2"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
 
--- StarterGui.ScreenGui.Frame.LocalScript
-G2L["3"] = Instance.new("LocalScript", G2L["2"]);
-
-
 -- StarterGui.ScreenGui.Frame.TextButton
-G2L["4"] = Instance.new("TextButton", G2L["2"]);
-G2L["4"]["BorderSizePixel"] = 0;
-G2L["4"]["TextSize"] = 14;
-G2L["4"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
-G2L["4"]["BackgroundColor3"] = Color3.fromRGB(124, 124, 124);
-G2L["4"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
-G2L["4"]["Size"] = UDim2.new(0, 158, 0, 24);
-G2L["4"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
-G2L["4"]["Text"] = [[Go!]];
-G2L["4"]["Position"] = UDim2.new(0.1821, 0, 0.78797, 0);
-
--- StarterGui.ScreenGui.Frame.TextButton.LocalScript
-G2L["5"] = Instance.new("LocalScript", G2L["4"]);
-
+G2L["3"] = Instance.new("TextButton", G2L["2"]);
+G2L["3"]["BorderSizePixel"] = 0;
+G2L["3"]["TextSize"] = 14;
+G2L["3"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["3"]["BackgroundColor3"] = Color3.fromRGB(124, 124, 124);
+G2L["3"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["3"]["Size"] = UDim2.new(0, 158, 0, 24);
+G2L["3"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["3"]["Text"] = [[Go!]];
+G2L["3"]["Position"] = UDim2.new(0.1821, 0, 0.78797, 0);
 
 -- StarterGui.ScreenGui.Frame.TextLabel
-G2L["6"] = Instance.new("TextLabel", G2L["2"]);
-G2L["6"]["BorderSizePixel"] = 0;
-G2L["6"]["BackgroundColor3"] = Color3.fromRGB(132, 132, 132);
-G2L["6"]["TextSize"] = 30;
-G2L["6"]["FontFace"] = Font.new([[rbxasset://fonts/families/Michroma.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
-G2L["6"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
-G2L["6"]["Size"] = UDim2.new(0, 200, 0, 50);
-G2L["6"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
-G2L["6"]["Text"] = [[FE Player TP]];
-G2L["6"]["Position"] = UDim2.new(0.09815, 0, -0.00379, 0);
+G2L["4"] = Instance.new("TextLabel", G2L["2"]);
+G2L["4"]["BorderSizePixel"] = 0;
+G2L["4"]["BackgroundColor3"] = Color3.fromRGB(132, 132, 132);
+G2L["4"]["TextSize"] = 30;
+G2L["4"]["FontFace"] = Font.new([[rbxasset://fonts/families/Michroma.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["4"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["4"]["Size"] = UDim2.new(0, 200, 0, 50);
+G2L["4"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["4"]["Text"] = [[FE Player TP]];
+G2L["4"]["Position"] = UDim2.new(0.09815, 0, -0.00379, 0);
 
 -- StarterGui.ScreenGui.Frame.TextBox
-G2L["7"] = Instance.new("TextBox", G2L["2"]);
-G2L["7"]["SelectionStart"] = 1;
-G2L["7"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
-G2L["7"]["BorderSizePixel"] = 0;
-G2L["7"]["TextSize"] = 14;
-G2L["7"]["BackgroundColor3"] = Color3.fromRGB(124, 124, 124);
-G2L["7"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
-G2L["7"]["Size"] = UDim2.new(0, 194, 0, 32);
-G2L["7"]["Position"] = UDim2.new(0.12048, 0, 0.42241, 0);
-G2L["7"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
-G2L["7"]["Text"] = [[Enter Username here!]];
-
--- StarterGui.ScreenGui.Frame.LocalScript
-local function C_3()
-local script = G2L["3"];
-	local UserInputService = game:GetService("UserInputService")
-	
-	local gui = script.Parent
-	
-	local dragging
-	local dragInput
-	local dragStart
-	local startPos
-	
-	local function update(input)
-		local delta = input.Position - dragStart
-		gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-	end
-	
-	gui.InputBegan:Connect(function(input)
-		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-			dragging = true
-			dragStart = input.Position
-			startPos = gui.Position
-	
-			input.Changed:Connect(function()
-				if input.UserInputState == Enum.UserInputState.End then
-					dragging = false
-				end
-			end)
-		end
-	end)
-	
-	gui.InputChanged:Connect(function(input)
-		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-			dragInput = input
-		end
-	end)
-	
-	UserInputService.InputChanged:Connect(function(input)
-		if input == dragInput and dragging then
-			update(input)
-		end
-	end)
-end;
-task.spawn(C_3);
--- StarterGui.ScreenGui.Frame.TextButton.LocalScript
-local function C_5()
-local script = G2L["5"];
-	local player = game.Players.LocalPlayer.PlayerGui.ScreenGui.Frame.TextBox.Text
-	tweento(player)
-	
-end;
-task.spawn(C_5);
-
+G2L["5"] = Instance.new("TextBox", G2L["2"]);
+G2L["5"]["SelectionStart"] = 1;
+G2L["5"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["5"]["BorderSizePixel"] = 0;
+G2L["5"]["TextSize"] = 14;
+G2L["5"]["BackgroundColor3"] = Color3.fromRGB(124, 124, 124);
+G2L["5"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["5"]["Size"] = UDim2.new(0, 194, 0, 32);
+G2L["5"]["Position"] = UDim2.new(0.12048, 0, 0.42241, 0);
+G2L["5"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["5"]["Text"] = [[Enter Username here!]];
 return G2L["1"], require;
+
+local UserInputService = game:GetService("UserInputService")
+local runService = (game:GetService("RunService"));
+
+local gui = game.Players.LocalPlayer.PlayerGui.ScreenGui.Frame
+
+local dragging
+local dragInput
+local dragStart
+local startPos
+
+function Lerp(a, b, m)
+	return a + (b - a) * m
+end;
+
+local lastMousePos
+local lastGoalPos
+local DRAG_SPEED = (8); -- // The speed of the UI darg.
+function Update(dt)
+	if not (startPos) then return end;
+	if not (dragging) and (lastGoalPos) then
+		gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, lastGoalPos.X.Offset, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, lastGoalPos.Y.Offset, dt * DRAG_SPEED))
+		return 
+	end;
+
+	local delta = (lastMousePos - UserInputService:GetMouseLocation())
+	local xGoal = (startPos.X.Offset - delta.X);
+	local yGoal = (startPos.Y.Offset - delta.Y);
+	lastGoalPos = UDim2.new(startPos.X.Scale, xGoal, startPos.Y.Scale, yGoal)
+	gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, xGoal, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, yGoal, dt * DRAG_SPEED))
+end;
+
+gui.InputBegan:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+		dragging = true
+		dragStart = input.Position
+		startPos = gui.Position
+		lastMousePos = UserInputService:GetMouseLocation()
+
+		input.Changed:Connect(function()
+			if input.UserInputState == Enum.UserInputState.End then
+				dragging = false
+			end
+		end)
+	end
+end)
+
+gui.InputChanged:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+		dragInput = input
+	end
+end)
+
+runService.Heartbeat:Connect(Update)
+
+local button = game.Players.LocalPlayer.PlayerGui.ScreenGui.Frame.TextButton
+button.MouseButton1Click:Connect(function()
+		local text = game.Players.LocalPlayer.PlayerGui.ScreenGui.Frame.TextBox.Text
+		tweento(text)
+	end)
